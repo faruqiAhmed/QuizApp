8a82ecfcf114a2aa17a2ce70a8d79829e0228507
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2024-08-10 14:44:12 +0600

    Initial commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Md Omar Faruq on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
